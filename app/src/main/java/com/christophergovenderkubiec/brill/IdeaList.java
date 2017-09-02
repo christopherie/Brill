803ff6812feb74939cdc41730e6d26fd039d90c5
christopherie <cgovenderkubiec@gmail.com>
2017-09-02 18:07:57 +0100

    Removed fab

--- app/src/main/java/com/christophergovenderkubiec/brill/IdeaList.java
+++ app/src/main/java/com/christophergovenderkubiec/brill/IdeaList.java
@@ -1,5 +1,6 @@
 package com.christophergovenderkubiec.brill;
 
+import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -48,10 +49,6 @@ public class IdeaList extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-
-
-
-
     }
 
 
