8c8b27ddaf64d4190ed073604663ef6e6c62e386
christopherie <cgovenderkubiec@gmail.com>
2017-09-02 11:34:01 +0100

    Register to Login intent

--- app/src/main/java/com/christophergovenderkubiec/brill/Register.java
+++ app/src/main/java/com/christophergovenderkubiec/brill/Register.java
@@ -1,7 +1,11 @@
 package com.christophergovenderkubiec.brill;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        final Button registerBtn = (Button)findViewById(R.id.button3);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(Register.this, Login.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
