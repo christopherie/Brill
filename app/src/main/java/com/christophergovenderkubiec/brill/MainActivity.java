730c653b86f62461443939efb14c7e00289f7aea
christopherie <cgovenderkubiec@gmail.com>
2017-09-02 10:48:43 +0100

    Added register intent

--- app/src/main/java/com/christophergovenderkubiec/brill/MainActivity.java
+++ app/src/main/java/com/christophergovenderkubiec/brill/MainActivity.java
@@ -1,7 +1,10 @@
 package com.christophergovenderkubiec.brill;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Register and Login Intents
+        final Button registerBtn = (Button)findViewById(R.id.button);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this, Register.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
