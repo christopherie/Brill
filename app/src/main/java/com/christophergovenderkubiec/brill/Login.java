e1b5cd15c566ab51a8874621cd23c2fb303d63dd
christopherie <cgovenderkubiec@gmail.com>
2017-09-02 14:43:30 +0100

    Added Profile fragment

--- app/src/main/java/com/christophergovenderkubiec/brill/Login.java
+++ app/src/main/java/com/christophergovenderkubiec/brill/Login.java
@@ -1,7 +1,10 @@
 package com.christophergovenderkubiec.brill;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final Button loginBtn = (Button)findViewById(R.id.button4);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = new Intent(Login.this, IdeaList.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
