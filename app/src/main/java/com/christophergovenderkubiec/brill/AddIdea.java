70d2c9be600f628dc1a000c9f5d38df4fb22c192
christopherie <cgovenderkubiec@gmail.com>
2017-09-03 23:02:01 +0100

    Added maven to gradle project file and play services to gradle module file

--- app/src/main/java/com/christophergovenderkubiec/brill/AddIdea.java
+++ app/src/main/java/com/christophergovenderkubiec/brill/AddIdea.java
@@ -1,7 +1,10 @@
 package com.christophergovenderkubiec.brill;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddIdea extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class AddIdea extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_idea);
+        Button saveIdeaBtn = (Button)findViewById(R.id.button5);
+        saveIdeaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent saveIdeaIntent = new Intent(AddIdea.this, AddLocation.class);
+                //startActivity(saveIdeaIntent);
+            }
+        });
     }
 }
