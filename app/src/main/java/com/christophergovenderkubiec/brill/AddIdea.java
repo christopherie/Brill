9d1071721cd535e60dc3cfcb24181bf38dc118c5
christopherie <cgovenderkubiec@gmail.com>
2017-09-13 21:51:37 +0100

    Added Database helper

--- app/src/main/java/com/christophergovenderkubiec/brill/AddIdea.java
+++ app/src/main/java/com/christophergovenderkubiec/brill/AddIdea.java
@@ -5,21 +5,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddIdea extends AppCompatActivity {
+
+    DatabaseOpenHelper databaseOpenHelper;
+    private Button saveIdeaBtn;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_idea);
-        Button saveIdeaBtn = (Button)findViewById(R.id.button5);
+        editText = (EditText)findViewById(R.id.editText6);
+        saveIdeaBtn = (Button)findViewById(R.id.button5);
+        databaseOpenHelper = new DatabaseOpenHelper(this);
         saveIdeaBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+            String newIdea = editText.getText().toString().trim();
+            if (editText.length() != 0) {
+                AddIdea(newIdea);
+                editText.setText("");
                 Intent saveIdeaIntent = new Intent(AddIdea.this, GetLocation.class);
                 startActivity(saveIdeaIntent);
-		        
+                finish();
+            } else {
+                toastMessage("You must add an idea!");
+            }
             }
         });
+    }
+
+    public void AddIdea(String newIdea) {
+        boolean insertIdea = databaseOpenHelper.addIdea(newIdea);
+        if (insertIdea) {
+            toastMessage("Idea saved!");
+        } else {
+            toastMessage("Idea not saved!");
+        }
+    }
+
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/christophergovenderkubiec/brill/DatabaseOpenHelper.java
@@ -0,0 +1,57 @@
+package com.christophergovenderkubiec.brill;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by predator on 13/09/2017.
+ */
+
+
+public class DatabaseOpenHelper extends SQLiteOpenHelper {
+    final static String TABLE_NAME = "brill_idea";
+    final static String IDEA = "idea";
+    final static String LOCATION = "location";
+    final static String ID = "id";
+
+    // Create table
+    final private static String CREATE_CMD =
+
+        "CREATE TABLE " + TABLE_NAME + "(" + ID
+                + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + IDEA + " TEXT NOT NULL, "
+                + LOCATION + " TEXT )";
+
+    final private static Integer VERSION = 1;
+    final private Context mContext;
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, TABLE_NAME, null, VERSION);
+        this.mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_CMD);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addIdea(String item) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(IDEA, item);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
